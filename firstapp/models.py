d35631508fdd4e9116bc5d150d777a66d8bdd28c
haxul <s.s.starodubov@yandex.ru>
2019-12-30 23:26:58 +0400

    init shop

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class PizzaShop(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Pizza-name")
+    description = models.TextField(max_length=400, verbose_name="Description")
+    rating = models.FloatField(default=0, verbose_name="Rating")
+    url = models.URLField(max_length=500, verbose_name="Shop Link")
+
+    def __str__(self):
+        return self.name
