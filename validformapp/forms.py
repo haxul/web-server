1e70dc02a87abe9313254174a16dd8a268b66012
haxul <s.s.starodubov@yandex.ru>
2020-01-07 16:35:39 +0400

    validation

--- validformapp/forms.py
+++ validformapp/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.core import validators
+
+
+def check_first_letter(word):
+    if word[0] != "A": raise forms.ValidationError("fuckin A")
 
 
 class NameForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_first_letter])
     email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
+    vmail = forms.EmailField(label="Enter your email again")
+    text = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(50)])
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data["email"]
+        vmail = cleaned_data["vmail"]
+        if email != vmail: raise forms.ValidationError("Not correct email")
+        return cleaned_data
--- validformapp/views.py
+++ validformapp/views.py
@@ -4,4 +4,11 @@ from . import forms
 
 def form_page(request):
     form = forms.NameForm()
-    return render(request, "validformapp/form_page.html", {})
+    if request.method == "POST":
+        form = forms.NameForm(request.POST)
+        if form.is_valid():
+            print("form is successfully")
+            print(f"name: {form.cleaned_data['name']}")
+            print(f"email: {form.cleaned_data['email']}")
+            print(f"text: {form.cleaned_data['text']}")
+    return render(request, "validformapp/form_page.html", {"form": form})
